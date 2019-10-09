b53563fe8a786793e66a98ce19b5f1872d6b0958
Mirco <mirco.meaz@gmail.com>
2019-10-10 00:29:16 +0200

    Initial Commit

---
+++ logistic_map/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  logistic_map
+//
+//  Created by Mirco Meazzo on 10/10/2019.
+//  Copyright © 2019 Mirco Meazzo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
