d1f62e8f47de56fbad0db639805ce2d754d97c93
Mirco <mirco.meaz@gmail.com>
2019-10-11 00:03:30 +0200

    Version 1.0 with iPython visualization

--- logistic_map/main.cpp
+++ logistic_map/main.cpp
@@ -7,9 +7,34 @@
 //
 
 #include <iostream>
+#include <fstream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    double r=2;
+    double x;
+    double xn;
+    double inc;
+    
+    
+    std::cout << "Insert x0 value: " << std::flush;
+    std::cin >> x;
+    std::cout << "Set incremental value: " << std::flush;
+    std::cin >> inc;
+    std::ofstream res;
+    
+    res.open("logistic_map.txt");
+    
+  if (res.is_open()) {
+        while (r<4) {
+//            Solve per r increments
+            xn= r*x*(1-x);
+            res << xn << "\t" << r << std::endl;
+            r=r+inc;
+            x=xn;
+        }
+    }
+    res.close();
+ 
     return 0;
 }
